abf4b254d0a5fb23209c647798e93b8a2f981117
alinaauezkhanovaa-eng <alinaauezkhanovaa@gmail.com>
2026-09-08 00:14:15 +0500

    Fix formatting and variable name in Email class

--- Email.java
+++ Email.java
@@ -4,9 +4,10 @@ public class Email {
     private final String  recipient,subject,body;
     private final boolean isHtml;
     Email(String recipient, String subject, String body, boolean isHtml) {
-        this.recipient=recipient;this.subject=subject;this.body=body;this.isHTML=isHtml;
+        this.recipient=recipient;this.subject=subject;this.body=body;this.isHtml=isHtml;
     }
-    @Override public String toString(){
+    @Override
+    public String toString(){
         return "Email[To:"+recipient+",Subject:"+subject+",Format:"+(isHtml?"HTML":"Plain Text")+",Body length:"+body.length()+"chars]";
     }
 }
